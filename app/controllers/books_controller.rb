944805223dba47e254e93e0bd1ce08c31399d597
KenInoue1911 <inouetkrk3@gmail.com>
2021-04-08 14:05:35 +0000

    2day

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,36 @@
 class BooksController < ApplicationController
-  def index
+  def show
+    @book = Book.find(params[:id])
   end
 
-  def show
+  def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def new
     @book = Book.new
   end
 
-  def edit
-  end
-  
   def create
     @book = Book.new(book_params)
-    book.save
+    @book.save
+    redirect_to book_path(@book)
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book)
+  end
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
     redirect_to books_path
   end
   
